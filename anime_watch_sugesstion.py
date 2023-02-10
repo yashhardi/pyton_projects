35911c165dc75ba96e4efd973363e5c4dfc1e337
yashhardi <121943137+yashhardi@users.noreply.github.com>
2023-02-10 11:14:36 +0530

    Add files via upload
    The above code helps to randomly select an anime from a list of given animes. It uses the choice function from the random module to randomly select an anime from the given list and print it. This is useful for when you want to randomly pick an anime to watch from a list of given options.

---
+++ anime_watch_sugesstion.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from random import choice
+#list of animes to watch 
+animes = ['Naruto','one punch','death note', 'Fullmetal Alchemist','Dragonball']
+
+#print random anime to watch 
+print(choice(animes))
+
